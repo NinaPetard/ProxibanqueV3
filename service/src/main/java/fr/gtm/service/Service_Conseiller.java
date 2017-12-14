71653510fa02392fe98bd8c787c012ffc0ec4b6f
Nina PETARD <nina.petard@gmail.com>
2017-12-14 09:54:11 +0100

    refonte service suite aux erreurs de compilation

--- service/src/main/java/fr/gtm/service/Service_Conseiller.java
+++ service/src/main/java/fr/gtm/service/Service_Conseiller.java
@@ -1,71 +1,26 @@
-package fr.gtm.Service;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.gtm.service;
 
 import fr.gtm.domaine.Client;
 import fr.gtm.domaine.Conseiller;
 import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
+/**
+ *
+ * @author adminl
+ */
 public class Service_Conseiller {
-
-    public void Service_Conseiller() {
-    }
-
-    public List<Client> listeClientsConseiller(Conseiller conseiller) {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("fr.gtm_proxibanquev3_PU");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-        List<Client> clients = new ArrayList<Client>();
-
-        try {
-            clients = conseiller.getClientList();
-            for (fr.gtm.domaine.Client client : clients) {
-                System.out.println(client.getNom());
-            }
-        } catch (Exception e) {
-            System.out.println("Pas de conseiller avec cet id");
-        }
-
-        em.close();
-        emf.close();
-
+    
+    public static ArrayList<Client> listerClientsConseiller(Conseiller conseiller){
+        ArrayList<Client> clients = new ArrayList<Client>();
+        
+        
         return clients;
-
+        
     }
-
-//    /**
-//     * Transfers the authentification demand from the servlet to the DAO
-//     *
-//     * @param login
-//     * @param password
-//     * @return
-//     */
-//    public boolean demanderconnexion(String login, String password) {
-//
-//        DAO_Verifications dao1 = new DAO_Verifications();
-//        boolean valeur = dao1.authConseiller(login, password);
-//
-//        return valeur;
-//
-//    }
-//
-//    /**
-//     * Transfers the list of an advisor's clients from DAO to frontend
-//     *
-//     * @param idconseiller
-//     * @return
-//     */
-//
-//    public ArrayList<Client> demanderclients(int idconseiller) {
-//        ArrayList<Client> clients = new ArrayList<Client>();
-//
-//        DAO_Client daoc = new DAO_Client();
-//        clients = daoc.getCliConseiller(idconseiller);
-//
-//        return clients;
-//    }
+    
 }

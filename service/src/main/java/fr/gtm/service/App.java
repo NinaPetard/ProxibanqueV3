0e119113f41644fc3cdc494f6fdc9ed0f4dfbaf7
Nina PETARD <nina.petard@gmail.com>
2017-12-14 09:54:11 +0100

    refonte service suite aux erreurs de compilation

--- service/src/main/java/fr/gtm/service/App.java
+++ service/src/main/java/fr/gtm/service/App.java
@@ -4,17 +4,15 @@ import fr.gtm.domaine.Client;
 import fr.gtm.domaine.Conseiller;
 import java.util.ArrayList;
 
-
-
-public class App {
-    public static void main( String[] args ){
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
         Conseiller conseiller = new Conseiller();
-        conseiller.setIdconseiller(2L);
-        
-        
-        ArrayList<Client> clients;
-        Service_Conseiller sc = new Service_Conseiller();
-        clients = sc.listeClientConseiller(conseiller);
-        
+        ArrayList<Client> clients = Service_Conseiller.listerClientsConseiller(conseiller);
     }
 }
